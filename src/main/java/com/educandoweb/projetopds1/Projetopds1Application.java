d51e2a083a6c6af6c468dab2f75d4de6aa4176d2
Felipe Braz <felipe.tbraz@gmail.com>
2019-08-20 22:29:36 -0300

    Projeto criado

---
+++ src/main/java/com/educandoweb/projetopds1/Projetopds1Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetopds1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetopds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetopds1Application.class, args);
+	}
+
+}
